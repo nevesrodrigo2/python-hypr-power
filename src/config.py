5ca4eecce6aecf9640c3a43537683a337c52f517
nevesrodrigo2 <nevesrodrigo2@gmail.com>
2025-08-18 21:53:31 +0100

    removed comment

--- src/config.py
+++ src/config.py
@@ -5,7 +5,6 @@ SRC_DIR = os.path.dirname(os.path.abspath(__file__))  # src directory
 BASE_DIR = os.path.dirname(SRC_DIR)  # project root
 RESOURCES_DIR = os.path.join(BASE_DIR, "resources")
 ICONS_DIR  = os.path.join(RESOURCES_DIR, "icons")  # icons directory
-# STYLES_DIR = os.path.join(SRC_DIR, "styles/")  # styles css directory
 STYLES_DIR = os.path.join(BASE_DIR, "src", "styles")
 
 # FONT
