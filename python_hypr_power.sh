5a3eb0b40552faaf43f56064cbf10d19e03ba6d2
nevesrodrigo2 <nevesrodrigo2@gmail.com>
2025-08-20 20:04:38 +0100

    refactored script to be used based on pipx

--- python_hypr_power.sh
+++ python_hypr_power.sh
@@ -2,13 +2,16 @@
 # Hyprland power menu replacement
 
 # Kill previous instance
-if pgrep -x "python_hypr_power" > /dev/null; then
-    pkill -x "python_hypr_power"
+if pgrep -x "python-hypr-power" > /dev/null; then
+    pkill -x "python-hypr-power"
     exit 0
 fi
 
 # Ensure library path is correct
 export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
 
+# Ensure pipx-installed binaries are in PATH
+export PATH="$HOME/.local/bin:$PATH"
+
 # Launch python_hypr_power
-/usr/local/bin/python_hypr_power &
+python-hypr-power &
